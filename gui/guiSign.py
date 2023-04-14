0993806d7b9e2f23714dd8ac45283c97d53e4ca0
RachmadHi <16520115@std.stei.itb.ac.id>
2023-04-14 21:16:25 +0700

    change RSA to rsa

--- gui/guiSign.py
+++ gui/guiSign.py
@@ -10,7 +10,7 @@ import tkinter.messagebox as tkmb
 import sys
 import guiLanding
 sys.path.append("../Kripto_3/src/")
-from RSA import sign_message, sign_file
+from rsa import sign_message, sign_file
 
 
 OUTPUT_PATH = Path(__file__).parent
